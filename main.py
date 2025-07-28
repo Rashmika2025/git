1eaf683a737889d73464f4233dd3bc7eab51f172
dev143A <devendrasenevirathna123@gmail.com>
2025-07-28 12:49:00 +0530

    comited by devendra to main branch

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+def multiplication(num1, num2):
+    return num1 * num2
+
+def division(num1, num2):
+    return num1 / num2
+
+def addition(num1,num2):
+    return num1 + num2
+
+def subtraction(num1,num2):
+    return num1 - num2
 
 def user_input():
     """Function to get user input."""
